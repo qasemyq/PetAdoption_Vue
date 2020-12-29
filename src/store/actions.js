15cc631ef29cd0fdeb5fc0caea31ca9edf1e2f6f
Ghader Ghasemi <68222268+qasemyq@users.noreply.github.com>
2020-12-29 20:23:45 +0100

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,4 @@
 export default {
-    // inja be jaye {species,pet} payload neveshtam
-    // payload mishe payload.species va payload.pet hast dagigan 
     addPet: ({commit}, payload) =>{
         commit('appendPet',payload)
     }
