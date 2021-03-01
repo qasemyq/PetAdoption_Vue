5567501a8ed5d95b8f68954a6c5f246463a307fd
Ghader Ghasemi <68222268+qasemyq@users.noreply.github.com>
2021-03-01 09:04:25 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Cats from '../views/Cats.vue'
+//
+import Cats from '../views/Cats.vue' 
+//
 import Dogs from '../views/Dogs.vue'
+// 
 import Pet from '../views/Pet.vue'
 
 Vue.use(VueRouter)
